44df0403c17b5e680313c90ff7c64923a9bdac9f
Daniel Juanda <danieljuanda276@gmail.com>
2018-06-06 17:14:31 +0700

    sidewall

--- material.js
+++ material.js
@@ -13,3 +13,18 @@ function createMat(scene)
 
   return mat;
 }
+function createMatSide(scene)
+{
+  var mat = new BABYLON.StandardMaterial("material",scene);
+
+  //scene.ambientColor = new BABYLON.Color3(1, 1, 1);
+  mat.diffuseTexture = new BABYLON.Texture("sidewall.jpg",scene);
+  //mat.diffuseColor = new BABYLON.Color3(1,0,1);
+  mat.specularColor = new BABYLON.Color3(0, 1, 0);
+  mat.specularPower = 25;
+  //mat.emissiveColor = new BABYLON.Color3(0.2, 0.2, 0.2);
+  mat.ambientColor = new BABYLON.Color3(0.23, 0.98, 0.98);
+  mat.alpha =1;
+
+  return mat;
+}
