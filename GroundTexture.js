e633866905bc5d186c0b431bd8cf8cece2fe8576
johanesr <johanesronaldo@gmail.com>
2018-06-07 09:29:09 +0700

    Redirect

--- GroundTexture.js
+++ GroundTexture.js
@@ -1,6 +1,6 @@
 function GroundTexture(scene){
   var mat = new BABYLON.StandardMaterial("mat", scene);
-  mat.diffuseTexture = new BABYLON.Texture("assets/ground5.jpg", scene);
+  mat.diffuseTexture = new BABYLON.Texture("assets/ground2.jpg", scene);
   mat.specularPower = 25
   mat.ambientColor = new BABYLON.Color3(0.23,1.25,0.48);
   mat.alpha = 1;
